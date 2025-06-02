6d3c0ca630abc948884f220124635fa196e51740
Eduardo Maximiano <emaximiano98@gmail.com>
2025-06-02 14:50:50 -0300

    feat: Importando componentes

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,18 @@
 import { Header } from "@components/Header";
 import { StyleSheet, View } from "react-native";
 import theme from "@theme/index";
+import { Highlight } from "@components/Highlight";
+import { CardGroup } from "@components/CardGroup";
 
 export function Groups() {
     
     return(
         <View style={styled.Container}>
             <Header />
+            <Highlight title="Turmas" subtitle="jogue com sua turma"/>
+
+            <CardGroup title="Ignite" />
+
         </View>
     )
 
