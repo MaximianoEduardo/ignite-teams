03903cf27d9a2d475e97c38a668f38e0d46714eb
Eduardo Maximiano <emaximiano98@gmail.com>
2025-06-03 15:42:17 -0300

    feat: Nova tela de grupos

---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@components/Button";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import theme from "@theme/index";
+import { UsersThree } from "phosphor-react-native";
+import { StyleSheet, View } from "react-native";
+
+export function NewGroup() {
+
+    return(
+        <View style={styled.container}>
+            <Header  showBackButton />
+            <View style={styled.content}>
+
+                <UsersThree 
+                    size={56} 
+                    color={theme.COLORS.GREEN_700}
+                    style={styled.icon}
+                />
+                <Highlight
+                    title="Nova Turma"
+                    subtitle="crie a turma para adicionar as pessoas"
+                />
+
+                <Button 
+                    title="Criar"
+                />
+            </View>
+        </View> 
+    );
+}
+
+const styled = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: theme.COLORS.GRAY_600,
+        padding: 24
+    },
+    content: {
+        flex: 1,
+        justifyContent: "center",
+    },
+    icon: {
+        alignSelf: "center"
+    }
+})
