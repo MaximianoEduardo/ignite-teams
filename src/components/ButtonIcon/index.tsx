b2381daf3cf645732dd304b714d246332c1d503e
Eduardo Maximiano <emaximiano98@gmail.com>
2025-06-04 13:57:01 -0300

    feat: Componente de icone

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,34 @@
+import { StyleSheet, TouchableOpacity } from "react-native";
+
+import { MaterialIcons } from "@expo/vector-icons";
+import theme from "@theme/index";
+export type ButtonIconTypeStyleProps = "PRIMARY" | "SECONDARY";
+
+type IProps = {
+    icon: keyof typeof MaterialIcons.glyphMap;
+    type?: ButtonIconTypeStyleProps
+}
+
+export function ButtonIcon ({type = "PRIMARY", icon}: IProps){
+
+    return (
+        <TouchableOpacity style={styled.container}>
+            <MaterialIcons 
+                name={icon}
+                color={type === "PRIMARY" ? theme.COLORS.GREEN_700 : theme.COLORS.RED}
+                size={32}
+            />
+        </TouchableOpacity>
+    );
+}
+
+const styled = StyleSheet.create({
+
+    container: {
+        width: 32,
+        height: 32,
+        justifyContent: "center",
+        alignItems: "center",
+        marginLeft: 12
+    }
+});
