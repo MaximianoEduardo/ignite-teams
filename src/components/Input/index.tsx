6e3c17d2bcab3797acf5343f8f65b8ba8eac5d4d
Eduardo Maximiano <emaximiano98@gmail.com>
2025-06-04 13:57:21 -0300

    refactor: Alternado logica para adequar layout

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,29 @@
 import theme from "@theme/index";
-import { StyleSheet, TextInput } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { AddressBook } from "phosphor-react-native";
+import { ButtonIcon } from "@components/ButtonIcon";
 
 
+type IProps = {
+    children?: React.JSX.Element;
+    placeholderText: string;
+    hasButton?: boolean;
+    iconName: keyof typeof MaterialIcons.glyphMap;
+}
 
-export function Input() {
+export function Input( props: IProps) {
     return(
-        <TextInput 
-            placeholder="Nome da turma" 
-            style={styled.container}
-            placeholderTextColor={theme.COLORS.GRAY_300}
-        >
-        </TextInput>
+        <View style={styled.container}>
+            <TextInput
+                autoCorrect={false}
+                placeholder={props.placeholderText} 
+                style={styled.container}
+                placeholderTextColor={theme.COLORS.GRAY_300}
+            />
+            <ButtonIcon icon={props.iconName} />
+        </View>
+        
     );
 }
 
@@ -18,13 +31,19 @@ const styled = StyleSheet.create({
 
     container: {
         flex: 1,
-        minHeight: 56,
-        maxHeight: 56,
+        justifyContent: "center",
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 16
+    },
+    input:{
         backgroundColor: theme.COLORS.GRAY_700,
         color: theme.COLORS.WHITE,
         fontFamily: theme.FONT_FAMILY.REGULAR,
         fontSize: theme.FONT_SIZE.MD,
+        flex: 1,
+        minHeight: 56,
+        maxHeight: 56,
         borderRadius: 6,
-        padding: 16
     }
 });
