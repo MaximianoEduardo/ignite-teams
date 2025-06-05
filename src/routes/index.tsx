0c9006940bf48461edf3de95203e9d35a4d03858
Eduardo Maximiano <emaximiano98@gmail.com>
2025-06-05 14:47:47 -0300

    fix: ajustando glith ao navegar

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,15 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { AppRoutes } from "./app.routes";
+import { View } from "react-native";
+import theme from "@theme/index";
 
 export function Routes(){
     return (
-        <NavigationContainer>
-            <AppRoutes />
-        </NavigationContainer>
+        <View style={{flex: 1, backgroundColor: theme.COLORS.GRAY_600}}>
+            <NavigationContainer>
+                <AppRoutes />
+            </NavigationContainer>
+        </View>
+        
     );
 }
