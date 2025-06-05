fbfa899a23c44985788fc8c9314aa2f944dc0fae
Eduardo Maximiano <emaximiano98@gmail.com>
2025-06-05 14:22:09 -0300

    feat: Implementando fn voltar

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,11 +2,18 @@ import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+import { useNavigation } from "@react-navigation/native";
 import theme from "@theme/index";
 import { UsersThree } from "phosphor-react-native";
 import { StyleSheet, View } from "react-native";
 
 export function NewGroup() {
+
+    const navigation = useNavigation();
+
+    function handleNew(){
+        navigation.navigate("players", { group: "grupo"})
+    }
 
     return(
         <View style={styled.container}>
@@ -23,11 +30,12 @@ export function NewGroup() {
                     subtitle="crie a turma para adicionar as pessoas"
                 />
 
-                <Input />
+                <Input placeholderText={"Nome da turma"} />
 
                 <Button
-                    title="Criar"
-                />
+                
+                    title="Criar" 
+                    onPressfn={() => handleNew() }/>
             </View>
         </View> 
     );
@@ -42,6 +50,7 @@ const styled = StyleSheet.create({
     content: {
         flex: 1,
         justifyContent: "center",
+        padding: 24
     },
     icon: {
         alignSelf: "center"
